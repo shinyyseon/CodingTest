5fb031d35b8c1ad31060c9dc42f1e7c24f281198
shinyyseon <57864253+shinyyseon@users.noreply.github.com>
2025-12-27 22:10:53 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 16.19 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String my_string, String overwrite_string, int s) {
+        String answer = "";
+        String start = my_string.substring(0, s);
+        String end = my_string.substring(s + overwrite_string.length(), my_string.length());
+        answer = start + overwrite_string + end;
+        return answer;
+    }
+}
